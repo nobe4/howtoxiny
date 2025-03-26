8eb99017a88a2bf00e4ee7c2083c019a4e63e12a
nobe4 <nobe4@users.noreply.github.com>
2025-03-26 19:19:04 +0100

    feat(css): update search styling (#41)

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
   const instance = new PagefindModularUI.Instance();
+
   instance.add(
     new PagefindModularUI.Input({
       inputElement: "#search",
@@ -12,14 +13,17 @@ window.addEventListener("DOMContentLoaded", () => {
     }),
   );
 
+  // This seems to help tailwind find the classes.
+  const liClasses = "m-2 border-b";
+  const aClasses = "flex justify-between items-center p-2 hover:text-accent";
   instance.add(
     new PagefindModularUI.ResultList({
       containerElement: "#results",
       placeholderTemplate: () => "<p>Loading ... </p>",
       resultTemplate: (result) => {
         return `
-        <li class="mb-2">
-            <a class="flex justify-between" href="${result.url}">
+        <li class="${liClasses}">
+            <a class="${aClasses}" href="${result.url}">
                 <span>${result.meta.title}</span>
                 <small>[${result.meta.kind}]</small>
             </a>
